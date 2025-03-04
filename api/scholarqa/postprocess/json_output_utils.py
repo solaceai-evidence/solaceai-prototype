a6c87488db0e35cce82149082a5d47b14c20be96
amanpreet692 <amanpreet.singh.x2@gmail.com>
2025-03-03 17:56:43 -0800

    edge case where citation id doesn't have authors and multiple such citations exist for a year.

--- api/scholarqa/postprocess/json_output_utils.py
+++ api/scholarqa/postprocess/json_output_utils.py
@@ -66,7 +66,10 @@ def resolve_ref_id(ref_str, ref_corpus_id, citation_ids):
         if citation_ids[ref_str]:
             rfsplits = ref_str.split(",")
             # in case of 2 (Doe et al., 2024), the one found later becomes (Doe et al._1, 2024) and so on...
-            ref_str_id = f"{rfsplits[0]}_{len(citation_ids[ref_str])}, {rfsplits[1]}"
+            if len(rfsplits) > 1:
+                ref_str_id = f"{rfsplits[0]}_{len(citation_ids[ref_str])}, {rfsplits[1]}"
+            else:
+                ref_str_id = f"{ref_str}_{len(citation_ids[ref_str])}"
         else:
             ref_str_id = ref_str
         citation_ids[ref_str][ref_corpus_id] = ref_str_id
