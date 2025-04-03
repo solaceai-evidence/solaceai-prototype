a729902d2a95ca3f417637fb1503f17b2412296d
Sergey Feldman <sergeyfeldman@gmail.com>
2025-04-02 19:37:18 -0700

    Adding sonnet 3.7

--- api/scholarqa/llms/constants.py
+++ api/scholarqa/llms/constants.py
@@ -5,6 +5,7 @@ GPT_4o = "openai/gpt-4o"
 GPT_4o_MINI = "openai/gpt-4o-mini"
 CLAUDE_3_OPUS = "anthropic/claude-3-opus-20240229"
 CLAUDE_35_SONNET = "anthropic/claude-3-5-sonnet-20241022"
+CLAUDE_37_SONNET = "anthropic/claude-3-7-sonnet-20250219"
 LLAMA_405_TOGETHER_AI = "together_ai/meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo"
 
 CompletionResult = namedtuple("CompletionCost",
