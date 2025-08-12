6722bcee18cce8f49eb30f6955c06bc413839ad2
Jesus Dominguez <jesus.dominguez@kcl.ac.uk>
2025-08-12 12:22:31 +0100

    changed the logo and added queries related to  project

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,9 +5,17 @@
  * @see https://github.com/allenai/varnish-mui
  */
 import React from 'react';
+/* getTheme: create MUI theme (colors, fonts, etc) for App. 
+    getRouterOverriddenTheme:  adjust theme for router-specific needs (like styling).
+    VarnishApp: main wrapper component from Varnish that applies the theme and global styles.
+    Varnish: AllenAI MUI-based design system. 
+*/
 import { getTheme, getRouterOverriddenTheme, VarnishApp } from '@allenai/varnish2';
+/* API for rendering app into the DOM (replaces ReactDOM.render) */
 import { createRoot } from 'react-dom/client';
+/* Provides client-side routing for the app, enabling navigation between pages without full reloads. */
 import { BrowserRouter } from 'react-router-dom';
+/* special link component for navigating to anchor  */
 import { HashLink } from 'react-router-hash-link';
 
 import { App } from './App';
--- ui/src/pages/About.tsx
+++ ui/src/pages/About.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 export const About = () => {
     useEffect(() => {
-        window.location.href = "https://allenai.org/blog/ai2-scholarqa"
+        window.location.href = "https://solaceai.org/"
     }, []);
     return (
         <div>redirecting...</div>
